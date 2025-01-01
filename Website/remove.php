fecf21c05019dc6c168904b09878dba83649c7af
Ammotracker <l407fsem@duck.com>
2025-01-01 15:18:01 -0500

    Remove Background Color
    Add changing background color to remove.php

--- Website/remove.php
+++ Website/remove.php
@@ -2,6 +2,15 @@
 
 include 'header.php';
 
+?>
+	<head>
+	<style>
+		body {
+			transition: background-color .5s ease;
+		}
+	</style>
+</head>
+<?php
 
 IF(!$_GET['act']==''){
 	$act = $_GET['act'];
@@ -20,16 +29,26 @@ if ($act=='remove'){
 	
 	if(mysqli_num_rows($Query) == 0){
 		print ("Not found or no Inventory found");
-		
+		$backgroundColor = 'red';
 	}else{
 		$sql3="UPDATE upc_main SET qty = qty - 1 WHERE upc = '$upc'";
 		//echo $sql3;
 		mysqli_query ($conn, $sql3);
 		echo "Removed";
+		$backgroundColor = 'green';
 	}
 }
 	
 	?>
+	<body bgcolor="<?= $backgroundColor; ?>" onload="changeBackgroundColor()">
+	
+	<script>
+		function changeBackgroundColor() {
+			setTimeout(function() {
+				document.body.style.backgroundColor = "white"; // Change to white after 5 seconds
+			}, 1500); // 1500ms = 1.5 seconds
+		}
+	</script>
 	
 	<form action="remove.php" method="post" class="submit">
 		<Table>
@@ -46,8 +65,4 @@ if ($act=='remove'){
 			<input type="hidden" id="act" name="act" value="remove">
 			<input type="submit">
 			</form>
-			<A href='index.php'>Home</A>
-<?php		
-
-?>
-
+			<A href='index.php'>Home</A></body>
