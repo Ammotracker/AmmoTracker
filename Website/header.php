47c9fbd1cd482807984e644ba81f41e94a1f111e
Ammotracker <l407fsem@duck.com>
2025-01-03 12:23:09 -0500

    Slightly Color Code Caliber
    Add logic to randomly colorcode the caliber

--- Website/header.php
+++ Website/header.php
@@ -7,4 +7,14 @@ session_start();
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
 
+
+
+ function getRandomBrightColor() {
+	 // Ensure the RGB components are not too low to avoid dark colors
+	 $red = mt_rand(128, 255);   // Red component (128-255 for brightness)
+	 $green = mt_rand(128, 255); // Green component (128-255 for brightness)
+	 $blue = mt_rand(128, 255);  // Blue component (128-255 for brightness)
+	 
+	 return sprintf('#%02X%02X%02X', $red, $green, $blue);
+ }
 ?>
--- Website/index.php
+++ Website/index.php
@@ -19,34 +19,75 @@ $sql = "
 ";
 $query = mysqli_query($conn, $sql);
 
+
+ 
+ 
 echo "<table border='2'><tr><td>";
 if (mysqli_num_rows($query) > 0) {
-	echo "<table border='1'>
-		<tr>
-			<th style='text-align: center'>Manufacturer</th>
-			<th style='text-align: center'>Bullet</th>
-			<th style='text-align: center'>Caliber</th>
-			<th style='text-align: center'>Grain</th>
-			<th style='text-align: center'>Qty per box</th>
-			<th style='text-align: center'>Boxes On Hand</th>
-			<th style='text-align: center'>Rounds On Hand</th>
-		</tr>";
-
-	while ($row = mysqli_fetch_array($query)) {
-		$qtyonhand = $row['qtybox'] * $row['qty'];
-		$totalammo += $qtyonhand; // Add rounds to total
-
-		echo "<tr>
-			<td style='text-align: center'>{$row['manufacturer']}</td>
-			<td style='text-align: center'>{$row['bullettype']}</td>
-			<td style='text-align: center'>{$row['caliber']}</td>
-			<td style='text-align: center'>{$row['grain']}</td>
-			<td style='text-align: center'>{$row['qtybox']}</td>
-			<td style='text-align: center'>{$row['qty']}</td>
-			<td style='text-align: center'>{$qtyonhand}</td>
-		</tr>";
+	
+	try {
+		$pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		// SQL query
+		$sql = "
+			SELECT 
+				upc_detail.name, 
+				upc_detail.grain, 
+				upc_detail.manufacturer, 
+				upc_detail.qtybox, 
+				upc_detail.bullettype, 
+				upc_detail.caliber, 
+				upc_main.qty
+			FROM upc_detail
+			INNER JOIN upc_main ON upc_detail.upc = upc_main.upc
+			ORDER BY upc_detail.caliber ASC
+		";
+		
+		$stmt = $pdo->query($sql);
+		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		// Get unique calibers and assign a random color to each
+		$caliberColors = [];
+		foreach ($rows as $row) {
+			if (!isset($caliberColors[$row['caliber']])) {
+				$caliberColors[$row['caliber']] = getRandomBrightColor();
+			}
+		}
+	} catch (PDOException $e) {
+		die("Database connection failed: " . $e->getMessage());
 	}
-	echo "</table><br><br>";
+
+	
+	?>
+	<table border="1" cellpadding="10" cellspacing="0">
+		<thead>
+			<tr>
+				<th>Manufacturer</th>
+				<th>Name</th>
+				<th>Grain</th>
+				<th>Qty/Box</th>
+				<th>Bullet Type</th>
+				<th>Caliber</th>
+				<th>Qty</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($rows as $row): ?>
+				<tr style="background-color: <?= $caliberColors[$row['caliber']]; ?>;">
+					<td><?= htmlspecialchars($row['manufacturer']); ?></td>
+					<td><?= htmlspecialchars($row['name']); ?></td>
+					<td><?= htmlspecialchars($row['grain']); ?></td>
+					<td><?= htmlspecialchars($row['qtybox']); ?></td>
+					<td><?= htmlspecialchars($row['bullettype']); ?></td>
+					<td><?= htmlspecialchars($row['caliber']); ?></td>
+					<td><?= htmlspecialchars($row['qty']); ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+	<?php
+	
 }
 
 echo "</td><td style='vertical-align: top'>";
@@ -79,6 +120,7 @@ if (mysqli_num_rows($query2) > 0) {
 			while ($row3 = mysqli_fetch_array($query3)) {
 				$rowcount = $row3['qtybox'] * $row3['qty'];
 				$totalrow += $rowcount; // Add to total rounds for this caliber
+				$totalammo += $rowcount;
 			}
 		}
 
